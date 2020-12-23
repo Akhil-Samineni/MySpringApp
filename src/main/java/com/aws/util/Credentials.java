7e6e376cfab9aff54c93428008f0bda99234aa70
Akhil-Samineni <51797530+Akhil-Samineni@users.noreply.github.com>
2020-12-23 12:37:12 -0600

    Update Credentials.java

--- src/main/java/com/aws/util/Credentials.java
+++ src/main/java/com/aws/util/Credentials.java
@@ -6,12 +6,12 @@ package com.aws.util;
  */
 public class Credentials {
 
-    public static final String access_key_id= "AKIA5O5TPMDFW53VA5P3";
+    public static final String access_key_id= "AKIA5XB5KVRW3LFBXLV7";
 
-    public static final String secret_access_key ="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";
+    public static final String secret_access_key ="jOqnXZZ1gZgk6ZqcsxXws5Frzd4/KRNk/rrjMSBi";
 
     public static final String region ="us-east-1";
 
-    public static final String keyArn="arn:aws:kms:us-east-1:925404979403:key/4f1d251e-3b96-41e1-ab99-ce66c31987b6";
+    public static final String keyArn="arn:aws:kms:us-east-1:942873947245:key/f94e7580-6b24-41b6-8059-c883d473271e";
 
 }
