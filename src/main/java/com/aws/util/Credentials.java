e2254d82515ebd3f4735479075bd3c5c4d84bea8
Akhil-Samineni <akhilsamineni47@gmail.com>
2020-12-22 20:20:56 -0600

    my bucket encryption commit

---
+++ src/main/java/com/aws/util/Credentials.java
@@ -0,0 +1,17 @@
+package com.aws.util;
+
+
+/**
+ * Created by Akhil on 23/07/2019.
+ */
+public class Credentials {
+
+    public static final String access_key_id= "AKIA5O5TPMDFW53VA5P3";
+
+    public static final String secret_access_key ="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";
+
+    public static final String region ="us-east-1";
+
+    public static final String keyArn="arn:aws:kms:us-east-1:925404979403:key/4f1d251e-3b96-41e1-ab99-ce66c31987b6";
+
+}
